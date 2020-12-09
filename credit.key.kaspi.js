37620d1c117e72716cf60c6adf9af35859cfbeff
dmitriyProgramer <46451948+dmitriyProgramer@users.noreply.github.com>
2020-12-09 21:52:23 +0500

    Update credit.key.kaspi.js

--- credit.key.kaspi.js
+++ credit.key.kaspi.js
@@ -4,15 +4,15 @@
 		sku = $(".t-store__prod-popup__title-wrapper .js-store-prod-sku.js-product-sku").text();
 		if( sku != '' ){
 			$('div[tt="В корзину"]').append('<div class="ks-widget" data-template="button" data-merchant-sku="'+sku+'" data-merchant-code="Belarusskoe" data-city="750000000"></div>');
-			sc = "<script id='credit'>(function(d, s, id) {";
-			sc+= "var js, kjs;";
-			sc+= "if (d.getElementById(id)) return;";
-			sc+= "js = d.createElement(s);";
-			sc+= "js.id = id;";
-			sc+= "js.src = 'https://kaspi.kz/kaspibutton/widget/ks-wi_ext.js';";
-			sc+= "kjs = document.getElementById('credit');";
-			sc+= "kjs.parentNode.insertBefore(js, kjs);";
-			sc+= "}(document, 'script', 'KS-Widget'));</script>";
+			sc = '<script id="credit">(function(d, s, id) {';
+			sc+= 'var js, kjs;';
+			sc+= 'if (d.getElementById(id)) return;';
+			sc+= 'js = d.createElement(s);';
+			sc+= 'js.id = id;';
+			sc+= 'js.src = "https://kaspi.kz/kaspibutton/widget/ks-wi_ext.js";';
+			sc+= 'kjs = document.getElementById("credit");';
+			sc+= 'kjs.parentNode.insertBefore(js, kjs);}(document, "script", "KS-Widget"));';
+			$('body').append(sc);
 			$('body').append(sc);
 			$('.t-store__prod-popup__title-wrapper').parent().find('select').attr('onchange', 'newSku()');
 			clearInterval(delInt1);
@@ -26,15 +26,14 @@ insertCreditKey = function() {
 		sku = $(".t-store__prod-popup__title-wrapper .js-store-prod-sku.js-product-sku").text();
 		if( sku != '' ){
 			$('div[tt="В корзину"]').append('<div class="ks-widget" data-template="button" data-merchant-sku="'+sku+'" data-merchant-code="Belarusskoe" data-city="750000000"></div>');
-			sc = "<script id='credit'>(function(d, s, id) {";
-			sc+= "var js, kjs;";
-			sc+= "if (d.getElementById(id)) return;";
-			sc+= "js = d.createElement(s);";
-			sc+= "js.id = id;";
-			sc+= "js.src = 'https://kaspi.kz/kaspibutton/widget/ks-wi_ext.js';";
-			sc+= "kjs = document.getElementById('credit');";
-			sc+= "kjs.parentNode.insertBefore(js, kjs);";
-			sc+= "}(document, 'script', 'KS-Widget'));</script>";
+			sc = '<script id="credit">(function(d, s, id) {';
+			sc+= 'var js, kjs;';
+			sc+= 'if (d.getElementById(id)) return;';
+			sc+= 'js = d.createElement(s);';
+			sc+= 'js.id = id;';
+			sc+= 'js.src = "https://kaspi.kz/kaspibutton/widget/ks-wi_ext.js";';
+			sc+= 'kjs = document.getElementById("credit");';
+			sc+= 'kjs.parentNode.insertBefore(js, kjs);}(document, "script", "KS-Widget"));';
 			$('body').append(sc);
 			$('.t-store__prod-popup__title-wrapper').parent().find('select').attr('onchange', 'newSku()');
 			clearInterval(delInt1);
