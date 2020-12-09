901a7d6e84b75f31116bbb6818bf92bb896d8cfe
dmitriyProgramer <46451948+dmitriyProgramer@users.noreply.github.com>
2020-12-09 18:57:25 +0500

    Update credit.key.kaspi.js

--- credit.key.kaspi.js
+++ credit.key.kaspi.js
@@ -18,7 +18,7 @@ $(document).ready(function() {
 
 
 newSku = function(){
-	$('.ks-widget').remove(); $('#credit').remove(); $('#ks-widget').remove(); 
+	$('.ks-widget').remove(); $('#credit').remove(); $('#KS-Widget').remove();
 	setTimeout(function(){
 		sku = $(".t-store__prod-popup__title-wrapper .js-store-prod-sku.js-product-sku").text();
 		$('div[tt="В корзину"]').append('<div class="ks-widget" data-template="button" data-merchant-sku="'+sku+'" data-merchant-code="Belarusskoe" data-city="750000000"></div>');
