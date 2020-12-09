cf2a0338ec05a1e24b52a75d89b99148d0355bcb
dmitriyProgramer <46451948+dmitriyProgramer@users.noreply.github.com>
2020-12-09 18:49:21 +0500

    Update credit.key.kaspi.js

--- credit.key.kaspi.js
+++ credit.key.kaspi.js
@@ -2,8 +2,15 @@ $(document).ready(function() {
 	setTimeout(function(){
 		sku = $(".t-store__prod-popup__title-wrapper .js-store-prod-sku.js-product-sku").text();
 		$('div[tt="В корзину"]').append('<div class="ks-widget" data-template="button" data-merchant-sku="'+sku+'" data-merchant-code="Belarusskoe" data-city="750000000"></div>');
-		$('body').append('<script id="credit"></script>');
-		$('#credit').html('(function(d, s, id) { var js, kjs;  if (d.getElementById(id)) return;  js = d.createElement(s);  js.id = id; js.src = "https://kaspi.kz/kaspibutton/widget/ks-wi_ext.js";  kjs = document.getElementById("credit"); kjs.parentNode.insertBefore(js, kjs); }(document, "script", "KS-Widget"));');
+		sc = "<script>(function(d, s, id) {";
+		sc+= "var js, kjs;";
+		sc+= "if (d.getElementById(id)) return;";
+		sc+= "js = d.createElement(s); js.id = id;";
+		sc+= "js.src = 'https://kaspi.kz/kaspibutton/widget/ks-wi_ext.js';";
+		sc+= "kjs = document.getElementsByTagName(s)[0];";
+		sc+= "kjs.parentNode.insertBefore(js, kjs);";
+		sc+= "}(document, 'script', 'KS-Widget'));</script>";
+		$('body').append(sc);
 		$('.t-store__prod-popup__title-wrapper').parent().find('select').attr('onchange', 'newSku()');
 	}, 1000);
 });
@@ -14,7 +21,14 @@ newSku = function(){
 	setTimeout(function(){
 		sku = $(".t-store__prod-popup__title-wrapper .js-store-prod-sku.js-product-sku").text();
 		$('div[tt="В корзину"]').append('<div class="ks-widget" data-template="button" data-merchant-sku="'+sku+'" data-merchant-code="Belarusskoe" data-city="750000000"></div>');
-		$('body').append('<script id="credit"></script>');
-		$('#credit').html('(function(d, s, id) { var js, kjs;  if (d.getElementById(id)) return;  js = d.createElement(s);  js.id = id; js.src = "https://kaspi.kz/kaspibutton/widget/ks-wi_ext.js";  kjs = document.getElementById("credit"); kjs.parentNode.insertBefore(js, kjs); }(document, "script", "KS-Widget"));');
+		sc = "<script>(function(d, s, id) {";
+		sc+= "var js, kjs;";
+		sc+= "if (d.getElementById(id)) return;";
+		sc+= "js = d.createElement(s); js.id = id;";
+		sc+= "js.src = 'https://kaspi.kz/kaspibutton/widget/ks-wi_ext.js';";
+		sc+= "kjs = document.getElementsByTagName(s)[0];";
+		sc+= "kjs.parentNode.insertBefore(js, kjs);";
+		sc+= "}(document, 'script', 'KS-Widget'));</script>";
+		$('body').append(sc);
 	}, 1000);
 }
