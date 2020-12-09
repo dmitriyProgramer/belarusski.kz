d6b1f34a631cfaec4deae12b39af9f6afc8b87bf
dmitriyProgramer <46451948+dmitriyProgramer@users.noreply.github.com>
2020-12-09 18:08:39 +0500

    Update credit.key.kaspi.js

--- credit.key.kaspi.js
+++ credit.key.kaspi.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
 		$('div[tt="В корзину"]').append('<div class="ks-widget" data-template="button" data-merchant-sku="'+sku+'" data-merchant-code="Belarusskoe" data-city="750000000"></div>');
 		$('body').append("<script id=\"credit\">(function(d, s, id) { var js, kjs;  if (d.getElementById(id)) return;  js = d.createElement(s);  js.id = id; js.src = 'https://kaspi.kz/kaspibutton/widget/ks-wi_ext.js';  kjs = document.getElementById('credit'); kjs.parentNode.insertBefore(js, kjs); }(document, 'script', 'KS-Widget'));</script>");
 		$('.t-store__prod-popup__title-wrapper').parent().find('select').attr('onchange', 'newSku()');
-	}, 200);
+	}, 1000);
 	$('body').append("newSku = "+newSku());
 });
 
@@ -15,5 +15,5 @@ newSku = function(){
 		sku = $(".t-store__prod-popup__title-wrapper .js-store-prod-sku.js-product-sku").text();
 		$('div[tt="В корзину"]').append('<div class="ks-widget" data-template="button" data-merchant-sku="'+sku+'" data-merchant-code="Belarusskoe" data-city="750000000"></div>');
 		$('body').append("<script id=\"credit\">(function(d, s, id) { var js, kjs;  if (d.getElementById(id)) return;  js = d.createElement(s);  js.id = id; js.src = 'https://kaspi.kz/kaspibutton/widget/ks-wi_ext.js';  kjs = document.getElementById('credit'); kjs.parentNode.insertBefore(js, kjs); }(document, 'script', 'KS-Widget'));</script>");
-	}, 200);
+	}, 1000);
 }
