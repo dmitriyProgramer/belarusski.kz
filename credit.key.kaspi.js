ef112990f64475099e8d73308684260bb09b7f50
dmitriyProgramer <46451948+dmitriyProgramer@users.noreply.github.com>
2020-12-10 07:57:07 +0500

    Update credit.key.kaspi.js

--- credit.key.kaspi.js
+++ credit.key.kaspi.js
@@ -4,18 +4,20 @@ $(document).ready(function() {
 		in_garbage = $('div[tt="В корзину"]');
 		if( sku != '' && in_garbage.length ){
 			$(in_garbage).append('<div class="ks-widget" data-template="button" data-merchant-sku="'+sku+'" data-merchant-code="Belarusskoe" data-city="750000000"></div>');
-			if( !$('script').is('#credit') ){
-				sc = '<script id="credit">(function(d, s, id) {';
-				sc+= 'var js, kjs;';
-				sc+= 'if (d.getElementById(id)) return;';
-				sc+= 'js = d.createElement(s);';
-				sc+= 'js.id = id;';
-				sc+= 'js.src = "https://kaspi.kz/kaspibutton/widget/ks-wi_ext.js";';
-				sc+= 'kjs = document.getElementById("credit");';
-				sc+= 'kjs.parentNode.insertBefore(js, kjs);}(document, "script", "KS-Widget"));';
-				$('body').append(sc);
-				$('.t-store__prod-popup__title-wrapper').parent().find('select').attr('onchange', 'newSku()');
-				clearInterval(delInt1);
+			if( $('div').is('.ks-widget') ){
+				if( !$('script').is('#credit') ){
+					sc = '<script id="credit">(function(d, s, id) {';
+					sc+= 'var js, kjs;';
+					sc+= 'if (d.getElementById(id)) return;';
+					sc+= 'js = d.createElement(s);';
+					sc+= 'js.id = id;';
+					sc+= 'js.src = "https://kaspi.kz/kaspibutton/widget/ks-wi_ext.js";';
+					sc+= 'kjs = document.getElementById("credit");';
+					sc+= 'kjs.parentNode.insertBefore(js, kjs);}(document, "script", "KS-Widget"));';
+					$('body').append(sc);
+					$('.t-store__prod-popup__title-wrapper').parent().find('select').attr('onchange', 'newSku()');
+					clearInterval(delInt1);
+				}
 			}
 		}
 	}, 1000);
@@ -28,22 +30,21 @@ newSku = function(){
 		in_garbage = $('div[tt="В корзину"]');
 		if( sku != '' && in_garbage.length ){
 			$(in_garbage).append('<div class="ks-widget" data-template="button" data-merchant-sku="'+sku+'" data-merchant-code="Belarusskoe" data-city="750000000"></div>');
-			if( !$('script').is('#credit') ){
-				sc = '<script id="credit">(function(d, s, id) {';
-				sc+= 'var js, kjs;';
-				sc+= 'if (d.getElementById(id)) return;';
-				sc+= 'js = d.createElement(s);';
-				sc+= 'js.id = id;';
-				sc+= 'js.src = "https://kaspi.kz/kaspibutton/widget/ks-wi_ext.js";';
-				sc+= 'kjs = document.getElementById("credit");';
-				sc+= 'kjs.parentNode.insertBefore(js, kjs);}(document, "script", "KS-Widget"));';
-				$('body').append(sc);
-				$('.t-store__prod-popup__title-wrapper').parent().find('select').attr('onchange', 'newSku()');
-				clearInterval(delInt2);
+			if( $('div').is('.ks-widget') ){
+				if( !$('script').is('#credit') ){
+					sc = '<script id="credit">(function(d, s, id) {';
+					sc+= 'var js, kjs;';
+					sc+= 'if (d.getElementById(id)) return;';
+					sc+= 'js = d.createElement(s);';
+					sc+= 'js.id = id;';
+					sc+= 'js.src = "https://kaspi.kz/kaspibutton/widget/ks-wi_ext.js";';
+					sc+= 'kjs = document.getElementById("credit");';
+					sc+= 'kjs.parentNode.insertBefore(js, kjs);}(document, "script", "KS-Widget"));';
+					$('body').append(sc);
+					$('.t-store__prod-popup__title-wrapper').parent().find('select').attr('onchange', 'newSku()');
+					clearInterval(delInt2);
+				}
 			}
 		}
 	}, 100);
 }
-
-
-
