c734f79b74391bb466e628d2b308098c3ed707ef
dmitriyProgramer <46451948+dmitriyProgramer@users.noreply.github.com>
2020-12-10 10:37:02 +0500

    Update credit.key.kaspi.js

--- credit.key.kaspi.js
+++ credit.key.kaspi.js
@@ -3,11 +3,7 @@ $(document).ready(function() {
 		sku = $(".t-store__prod-popup__title-wrapper .js-store-prod-sku.js-product-sku").text();
 		in_garbage = $('.t-store__prod-popup__btn-wrapper');
 		if( sku != '' && in_garbage.length ){
-			if( !$('div').is('.key__fon') ){
-				$(in_garbage).append('<div class="key__fon"></div>');
-				$('.key__fon').css({"display":"none", "width":"160px", "height":"44px", "background":"#f14635", "background":"linear-gradient(#f76051 2%,#fb806d 5%,#f76051 5%,#f14635)", "border-radius":"4px"});
-			}
-			$('.key__fon').append('<div class="ks-widget" data-template="button" data-merchant-sku="'+sku+'" data-merchant-code="Belarusskoe" data-city="750000000"></div>');
+			$(in_garbage).append('<div class="ks-widget" data-template="button" data-merchant-sku="'+sku+'" data-merchant-code="Belarusskoe" data-city="750000000"></div>');
 			if( $('div').is('.ks-widget') ){
 				if( !$('script').is('#credit') ){
 					sc = '<script id="credit">(function(d, s, id) {';
@@ -20,7 +16,6 @@ $(document).ready(function() {
 					sc+= 'kjs.parentNode.insertBefore(js, kjs);}(document, "script", "KS-Widget"));';
 					$('body').append(sc);
 					$('.t-store__prod-popup__title-wrapper').parent().find('select').attr('onchange', 'newSku()');
-					display__key();
 					clearInterval(delInt1);
 				}
 			}
@@ -47,12 +42,8 @@ newSku = function(){
 		sku = $(".t-store__prod-popup__title-wrapper .js-store-prod-sku.js-product-sku").text();
 		in_garbage = $('.t-store__prod-popup__btn-wrapper');
 		if( sku != '' && in_garbage.length ){
-			if( !$('div').is('.key__fon') ){
-				$(in_garbage).append('<div class="key__fon"></div>');
-				$('.key__fon').css({"display":"none", "width":"160px", "height":"44px", "background":"#f14635", "background":"linear-gradient(#f76051 2%,#fb806d 5%,#f76051 5%,#f14635)", "border-radius":"4px"});
-			}
 			if( !$('div').is('.ks-widget') ){
-				$('.key__fon').append('<div class="ks-widget" data-template="button" data-merchant-sku="'+sku+'" data-merchant-code="Belarusskoe" data-city="750000000"></div>');
+				$(in_garbage).append('<div class="ks-widget" data-template="button" data-merchant-sku="'+sku+'" data-merchant-code="Belarusskoe" data-city="750000000"></div>');
 			}
 			if( $('div').is('.ks-widget') ){
 				if( !$('script').is('#credit') ){
@@ -66,23 +57,9 @@ newSku = function(){
 					sc+= 'kjs.parentNode.insertBefore(js, kjs);}(document, "script", "KS-Widget"));';
 					$('body').append(sc);
 					$('.t-store__prod-popup__title-wrapper').parent().find('select').attr('onchange', 'newSku()');
-					display__key();
 					clearInterval(delInt2);
 				}
 			}
 		}
 	}, 1000);
-}
-
-window.priDelInt4 = 0;
-
-display__key = function(){
-	window.delInt4 = setInterval(function(){
-		if( $('.ks-widget iframe').length ){
-			$('.key__fon').css({"display":""});
-			clearInterval(delInt4);
-		}
-		if( priDelInt4 == 100 ){ clearInterval(delInt4); }
-		priDelInt4++;
-	}, 100);
 }
