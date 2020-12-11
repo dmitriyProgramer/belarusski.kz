736c764ecf5a87c70af32eb6439ddfbe4f561798
dmitriyProgramer <46451948+dmitriyProgramer@users.noreply.github.com>
2020-12-11 18:27:06 +0500

    Update balance.js

--- balance.js
+++ balance.js
@@ -1,3 +1,9 @@
-$(document).ready(function() {
-	console.log('asd');
-});
+q_balance = function() {
+	if( $('#rec260174013 form input[type="hidden"][name="Phone"]').val().replace(/\+7/, '').replace(/[^\d]/g, '').length != 10 ){
+	    $('#rec260174013 form button').click();
+	}else{
+	    $('#rec260174013 .js-errorbox-all.t-form__errorbox-wrapper').css({"display":"none"});
+	    $('#rec260174013 .t-input-error').text('');
+	    $('#rec260174013 .t-form__inputsbox [data-input-lid]').attr('class', 't-input-group t-input-group_ph');
+	}
+};
