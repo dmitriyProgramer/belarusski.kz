1cac891021222bea38dda5c90245499b25282f9f
dmitriyProgramer <46451948+dmitriyProgramer@users.noreply.github.com>
2020-12-11 20:12:09 +0500

    Update balance.js

--- balance.js
+++ balance.js
@@ -6,8 +6,8 @@ $(document).ready(function() {
 			$('#rec260174013 form button').parent().append('<span class="'+_class+'" style="'+style+'" onclick="q_balance()">УЗНАТЬ</span>');
 			$('#rec260174013 form button').css({"display":"none"});
             $('#rec260174013 form button').parent().parent().after('<div class="t-form__inputsbox balance" style="margin-top:18px;"></div>');
-			$('#rec260174013 .balance').append('<div class="bal_name"><span style="display:inline-block;width:50%;">Имя:</span><span></span></div>');
-			$('#rec260174013 .balance').append('<div class="bal_total"><span style="display:inline-block;width:50%;">Остаток баллов на:</span><span></span></div>');
+			$('#rec260174013 .balance').append('<div class="bal_name"><span style="display:inline-block;width:20%;">Имя:</span><span style="display:inline-block;width:79%;text-align:end;"></span></div>');
+			$('#rec260174013 .balance').append('<div class="bal_total"><span style="display:inline-block;width:65%;">Остаток баллов на:</span style="display:inline-block;width:34%;text-align:end;"><span></span></div>');
 		    clearInterval(delInt);
 		}
 	}, 1000);
@@ -63,6 +63,7 @@ q_balance = function() {
 					}
                     $($('.bal_name span')[1]).text(name);
                     $($('.bal_total span')[1]).text(XFormatPrice(arr['balance']['RESIDUE']));
+                    $($('.bal_total span')[0]).text('Остаток баллов на: '+arr['data']);
 		        }
 		    }
 		});
